8826830721b6528dde965c22d37f207903889f00
Alexandre Bourdeaud'hui <alexandre.bourdeaudhui@gmail.com>
2022-06-18 00:21:07 +0200

    Add new test

--- FizzBuzz/src/index.test.ts
+++ FizzBuzz/src/index.test.ts
@@ -18,4 +18,8 @@ describe('FizzBuzz', () => {
   it('should return Buzz when input is 5', () => {
     expect(fizzBuzz(5)).toBe('Buzz');
   });
+
+  it('should return input when input is not divisible by 3 or 5', () => {
+    expect(fizzBuzz(1)).toBe(1);
+  });
 })
